ec1d852d6e437fab12cdca80dfd630259cbf9462
girinssh <tngus5851@naver.com>
2022-09-30 02:22:09 +0900

    Add Logs and Check Permissions for Bluetooth and Record Audio

--- app/src/main/java/com/capstone/breathdatacollector/BluetoothHelper.java
+++ app/src/main/java/com/capstone/breathdatacollector/BluetoothHelper.java
@@ -1,19 +1,28 @@
 package com.capstone.breathdatacollector;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -40,6 +49,7 @@ public class BluetoothHelper {
             @Override
             public void onServiceConnected(int profile, BluetoothProfile proxy) {
                 if (profile == BluetoothProfile.HEADSET) {
+                    Log.d(TAG, "Headset Connected");
                     headset = (BluetoothHeadset) proxy;
                 }
             }
@@ -47,6 +57,7 @@ public class BluetoothHelper {
             @Override
             public void onServiceDisconnected(int profile) {
                 if (profile == BluetoothProfile.HEADSET) {
+                    Log.d(TAG, "Headset Disconnected");
                     headset = null;
                 }
             }
@@ -60,37 +71,120 @@ public class BluetoothHelper {
         return instance;
     }
 
-    public boolean checkBluetoothEnabled(AppCompatActivity activity){
-        if (adapter == null) {
+    public static boolean checkBluetoothEnabled(AppCompatActivity activity){
+        if(instance == null){
+            instance = new BluetoothHelper();
+        }
+        if (instance.adapter == null) {
             return false;
         }
-        if (!adapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+
+        ActivityResultLauncher<String> permissionLauncher;
+
+        ActivityResultCallback<Boolean> arc;
+
+
+
+        if(activity.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED){
+            arc = (isGranted) -> {
+                    if (!isGranted) {
+                        new AlertDialog.Builder(activity.getApplicationContext())
+                                .setTitle("블루투스 접근 권한")
+                                .setMessage("앱을 사용하시려면, 블루투스 권한을 허용해 주세요.")
+                                .setPositiveButton("확인", (DialogInterface dialog, int which)->{
+                                        Intent intent = new Intent();
+                                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package",
+                                                BuildConfig.APPLICATION_ID, null);
+                                        intent.setData(uri);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        activity.startActivity(intent);
+                                    }
+                                )
+                                .create()
+                                .show();
+                    }
+                };
+            permissionLauncher = activity.registerForActivityResult(new ActivityResultContracts.RequestPermission(), arc);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                permissionLauncher.launch(Manifest.permission.BLUETOOTH_CONNECT);
+            }
+        }
+
+        if(activity.checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            arc = (isGranted) ->{
+                    if (!isGranted) {
+                        new AlertDialog.Builder(activity.getApplicationContext())
+                                .setTitle("녹음 권한")
+                                .setMessage("앱을 사용하시려면, 녹음 권한을 허용해 주세요.")
+                                .setPositiveButton("확인", (DialogInterface dialog, int which) -> {
+                                        Intent intent = new Intent();
+                                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package",
+                                                BuildConfig.APPLICATION_ID, null);
+                                        intent.setData(uri);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        activity.startActivity(intent);
+                                    }
+                                )
+                                .create()
+                                .show();
+                    }
+                };
+            permissionLauncher = activity.registerForActivityResult(new ActivityResultContracts.RequestPermission(), arc);
+            permissionLauncher.launch(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (!instance.adapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
             ActivityResultLauncher<Intent> launcher = activity.registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
-                    result -> {});
+                    result -> {
+                        if(result.getResultCode() != RESULT_OK){
 
+                            new AlertDialog.Builder(activity.getApplicationContext())
+                                    .setTitle("블루투스 활성화")
+                                    .setMessage("데이터 수집을 위해서, 블루투스를 활성화해 주세요.")
+                                    .setPositiveButton("확인", (DialogInterface dialog, int which) -> {
+                                            Intent intent = new Intent();
+                                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                            Uri uri = Uri.fromParts("package",
+                                                    BuildConfig.APPLICATION_ID, null);
+                                            intent.setData(uri);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            activity.startActivity(intent);
+                                        }
+                                    )
+                                    .create()
+                                    .show();
+                        }
+                    });
             launcher.launch(enableBtIntent);
-            return adapter.isEnabled();
+
+            return instance.adapter.isEnabled();
         }
         return true;
     }
 
     public boolean startBluetoothHeadset(Context context) {
+        Log.d(TAG, "start Bluetooth Headset Method");
 
         context.registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
                 if(AudioManager.SCO_AUDIO_STATE_CONNECTED == state){
-                    context.unregisterReceiver(this);
+                    Log.d(TAG, "onReceive, SCO_AUDIO_CONNECTED");
 
                     Toast.makeText(context, "Bluetooth Headset Connected", Toast.LENGTH_SHORT).show();
-
                     adapter.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET);
+                    context.unregisterReceiver(this);
                 }
             }
         }, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED));
+
+        Log.d(TAG, "start Bluetooth Headset Method End");
 
         return true;
     }
