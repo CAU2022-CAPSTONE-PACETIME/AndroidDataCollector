77a7cea37c8eb56bb71f6edeec60d9017828845a
girinssh <tngus5851@naver.com>
2022-09-29 23:44:39 +0900

    Make BluetoothHelper Singletone Pattern

--- app/src/main/java/com/capstone/breathdatacollector/BluetoothHelper.java
+++ app/src/main/java/com/capstone/breathdatacollector/BluetoothHelper.java
@@ -1,23 +1,41 @@
 package com.capstone.breathdatacollector;
 
 import android.Manifest;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothHeadset;
 import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 
 public class BluetoothHelper {
+    private static final String TAG = "BLUETOOTH_HELPER";
 
     private BluetoothHeadset headset;
     private BluetoothAdapter adapter;
+    private Boolean isEnabled = false;
 
     private BluetoothProfile.ServiceListener profileListener;
 
-    public BluetoothHelper() {
+    private static BluetoothHelper instance;
+
+    private BluetoothHelper() {
+        adapter = BluetoothAdapter.getDefaultAdapter();
         profileListener = new BluetoothProfile.ServiceListener() {
             @Override
             public void onServiceConnected(int profile, BluetoothProfile proxy) {
@@ -33,39 +51,51 @@ public class BluetoothHelper {
                 }
             }
         };
-
-
     }
 
-    public boolean startBluetoothHeadset(Context context) {
-        adapter = BluetoothAdapter.getDefaultAdapter();
+    public static BluetoothHelper getInstance(){
+        if(instance == null){
+            instance = new BluetoothHelper();
+        }
+        return instance;
+    }
 
+    public boolean checkBluetoothEnabled(AppCompatActivity activity){
         if (adapter == null) {
             return false;
         }
-
         if (!adapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            ActivityResultLauncher<Intent> launcher = activity.registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {});
 
-                return false;
-            }
-            context.startActivity(enableBtIntent);
+            launcher.launch(enableBtIntent);
+            return adapter.isEnabled();
         }
+        return true;
+    }
 
-        adapter.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET);
+    public boolean startBluetoothHeadset(Context context) {
+
+        context.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int state = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1);
+                if(AudioManager.SCO_AUDIO_STATE_CONNECTED == state){
+                    context.unregisterReceiver(this);
+
+                    Toast.makeText(context, "Bluetooth Headset Connected", Toast.LENGTH_SHORT).show();
+
+                    adapter.getProfileProxy(context, profileListener, BluetoothProfile.HEADSET);
+                }
+            }
+        }, new IntentFilter(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED));
+
         return true;
     }
 
     public void disconnectHeadset(){
-        adapter.closeProfileProxy(BluetoothProfile.HEADSET, headset);;
+        adapter.closeProfileProxy(BluetoothProfile.HEADSET, headset);
     }
 }
