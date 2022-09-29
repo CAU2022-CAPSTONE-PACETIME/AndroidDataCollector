f772373f6519761bf92500742f7ca7ec901a25d4
alsgur7859 <alsgur7859@naver.com>
2022-09-29 20:29:14 +0900

    add buttons

--- app/src/main/java/com/capstone/breathdatacollector/MainActivity.java
+++ app/src/main/java/com/capstone/breathdatacollector/MainActivity.java
@@ -1,8 +1,10 @@
 package com.capstone.breathdatacollector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "저는 확인1이에요.", Toast.LENGTH_LONG).show(); // Toast는 간단한 메세지를 잠깐 보여주는 역할을 수행한다.
     }
 }
